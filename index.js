fab44ced8097bc51c653648313cbd0ba3770f768
aleksandrchuyko <chao.mess@gmail.com>
2024-03-04 17:59:41 +0200

    median

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
 
-const dbPath = path.resolve('./hello.txt');
+const dbPath = path.resolve('./10m.txt');
 
 async function parseFile() {
   try {
@@ -15,6 +15,8 @@ async function parseFile() {
     let avg = null;
     let incr_arr = [];
     let incr_temp_arr = [];
+    let decr_arr = [];
+    let decr_temp_arr = [];
 
     readInterface = readline.createInterface({
       input: fs.createReadStream(dbPath),
@@ -40,13 +42,29 @@ async function parseFile() {
         if (incr_temp_arr.length > 0) {
           if (num > incr_temp_arr[incr_temp_arr.length - 1]) {
             incr_temp_arr.push(num);
-            if (incr_temp_arr.length > incr_arr.length)
+
+            if (incr_temp_arr.length > incr_arr.length) {
               incr_arr = [...incr_temp_arr];
+            }
           } else {
-            incr_temp_arr = [];
+            incr_temp_arr = [num];
           }
         } else {
           incr_temp_arr.push(num);
+        }
+
+        if (decr_temp_arr.length > 0) {
+          if (num < decr_temp_arr[decr_temp_arr.length - 1]) {
+            decr_temp_arr.push(num);
+
+            if (decr_temp_arr.length > decr_arr.length) {
+              decr_arr = [...decr_temp_arr];
+            }
+          } else {
+            decr_temp_arr = [num];
+          }
+        } else {
+          decr_temp_arr.push(num);
         }
       }
     }
@@ -57,28 +75,54 @@ async function parseFile() {
     let dif = null;
     let med = null;
 
-    readInterface = readline.createInterface({
-      input: fs.createReadStream(dbPath),
-    });
+    async function oddQuantityMedian() {
+      readInterface = readline.createInterface({
+        input: fs.createReadStream(dbPath),
+      });
 
-    for await (const line of readInterface) {
-      num = +line.trim();
-      if (Number.isInteger(num) && line !== '') {
-        bot = Math.abs(min - num);
-        top = Math.abs(max - num);
-        dif = Math.abs(top - bot);
-        if (delta === null) {
-          med = num;
-          delta = dif;
-        } else if (dif < delta) {
-          med = num;
-          delta = dif;
+      for await (const line of readInterface) {
+        num = +line.trim();
+        if (Number.isInteger(num) && line !== '') {
+          bot = Math.abs(min - num);
+          top = Math.abs(max - num);
+          dif = Math.abs(top - bot);
+          if (delta === null) {
+            med = num;
+            delta = dif;
+          } else if (dif < delta) {
+            med = num;
+            delta = dif;
+          }
         }
       }
     }
 
-    console.log(min, max, avg, med);
+    async function evenQuantityMedian() {
+      readInterface = readline.createInterface({
+        input: fs.createReadStream(dbPath),
+      });
+
+      for await (const line of readInterface) {
+        num = +line.trim();
+        if (Number.isInteger(num) && line !== '') {
+          bot = Math.abs(min - num);
+          top = Math.abs(max - num);
+          dif = Math.abs(top - bot);
+          if (delta === null) {
+            med = num;
+            delta = dif;
+          } else if (dif < delta) {
+            med = num;
+            delta = dif;
+          }
+        }
+      }
+    }
+
+    await oddQuantityMedian();
+    console.log(min, max, avg, med, i);
     console.log(incr_arr);
+    console.log(decr_arr);
   } catch (error) {
     console.log(error);
   }
